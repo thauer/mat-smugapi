531a6bc3c73023f7d512e92bdd029ee9a7ff8846
Tamás Hauer <Tamas.Hauer@prodema.ch>
2014-03-14 16:46:21 +0100

    test

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -6,10 +6,12 @@ import requests
 class RequestsTestSuite(unittest.TestCase):
     """Requests test cases."""
 
-    def testRequestSimpleGet(self):
+    def testRequestConnection(self):
         r = requests.get('https://github.com/timeline.json')
-        print r.text
-        assert True
+        self.assertEqual(r.status_code,200)
+
+    def testRequestGet(self):
+        r = requests.get('https://api.smugmug.com/services/api/json/1.3.0/')
 
 if __name__ == '__main__':
     unittest.main()
